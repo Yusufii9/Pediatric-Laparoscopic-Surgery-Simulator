2d7e7647e0adc67e22d8fcb2597521e9011c8a10
Youssef Megahed <98136322+Yusufii9@users.noreply.github.com>
2024-01-19 19:25:35 -0500

    Update seg_data_to_subtasks.py

--- Software/Working-Demo/seg_data_to_subtasks.py
+++ Software/Working-Demo/seg_data_to_subtasks.py
@@ -7,15 +7,15 @@ warnings.filterwarnings('ignore')
 
 class DataSegmentation:
     def __init__(self):
-        self.reference_file = 'CleanedSensorData_2.csv'
-        self.user_file_path = filedialog.askopenfilename()
-        self.labels_file = 'Labelled Videos.xlsx'
+        self.reference_file = 'CleanedSensorData_8.csv'
+        self.user_file_path = filedialog.askopenfilename(title="Choose the CSV Converted Sensor Data File")  # choose 7
+        self.labels_file = 'Demo Labelled Video.xlsx'
 
     def load_data(self):
         self.data_video_10 = pd.read_csv(self.reference_file)
         self.data_video_9 = pd.read_csv(self.user_file_path)
-        self.labels_video_10 = pd.read_excel(self.labels_file, sheet_name='Video 10')
-        self.labels_video_9 = pd.read_excel(self.labels_file, sheet_name='Video 9')
+        self.labels_video_10 = pd.read_excel(self.labels_file, sheet_name='YM Trial one')
+        self.labels_video_9 = pd.read_excel(self.labels_file, sheet_name='AM Trail one')
 
     def process_segments(self, data, labels):
         segments = []
@@ -35,8 +35,8 @@ class DataSegmentation:
         user_segments = self.process_segments(self.data_video_9, self.labels_video_9)
 
         task_ranges = [(0, 9), (11, 20), (22, 32)]
-        self.combine_and_save_segments(reference_segments, "Video10_Reference", task_ranges)
-        self.combine_and_save_segments(user_segments, "Video9_user", task_ranges)
+        self.combine_and_save_segments(reference_segments, "Demo_Reference", task_ranges)
+        self.combine_and_save_segments(user_segments, "Demo_user", task_ranges)
 
 
 if __name__ == "__main__":
